670f963f750869642dbdf4f0b56136fd3fa0b72e
Nazareth <50680385+N4za@users.noreply.github.com>
2020-08-15 20:42:06 -0500

    Base en la nube

---
+++ sql/remoto.sql
@@ -0,0 +1,18 @@
+USE xq2g14cu2ylsahky;
+
+CREATE TABLE alumnos(
+    id_alumno INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    matricula INT(11) NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
+    onelastname VARCHAR(50) NOT NULL,
+    twolastname VARCHAR(50) NOT NULL,
+    edad TINYINT NOT NULL,
+    borndate DATE NOT NULL,
+    sex VARCHAR(10) NOT NULL,
+    estado VARCHAR(50) NOT NULL
+) ENGINE= InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO alumnos(matricula, nombre, onelastname, twolastname, edad, borndate, sex, estado)
+VALUES
+(1718110385, 'Nazareth', 'Muñoz', 'Ruiz', 20, '2000-04-04', 'Femenino', 'Hidalgo'),
+(1718110411, 'Maria Fernanda', 'Reyes', 'Lopez', 20, '1999-04-23', 'Femenino', 'Hidalgo');
